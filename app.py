7ddee7068c02eabc2f9b2d62b04ea6c9993e00ff
Ritesh <ritsmahale18@gmail.com>
2024-08-07 10:41:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langdetect import detect
 # Set the page configuration
 st.set_page_config(
     page_title="Marathi News Generator",  # Title of the web page with smiley
-    page_icon=":smiley:",  # Emoji or path to a favicon
     layout="centered"  # Layout of the page
 )
 
