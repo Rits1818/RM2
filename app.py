2a56bbd0b4015913771bf26c00c17745bef91952
Ritesh <ritsmahale18@gmail.com>
2024-08-08 19:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ st.set_page_config(
     layout="centered"  # Layout of the page
 )
 
+
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
  
 
 
